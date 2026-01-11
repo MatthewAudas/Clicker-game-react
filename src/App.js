fffe996de3c66b564a109d774f220d308b067ea9
MatthewAudas <100880161+MatthewAudas@users.noreply.github.com>
2026-01-11 03:07:40 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import './App.css';
+import './Shop.js';
+import Shop from './Shop.js';
 let x = 1;
 let priceofItem = 10;
 let priceofItem2 = 20;
@@ -13,23 +15,6 @@ function PressButton({ onPress, children }) {
   );
 }
 
-function PressButton2({ onPress, children }) {
-  // Exposes an `onPress` prop (React Native style) and maps it to a web `onClick`.
-  return (
-    <button type="button" onClick={onPress}>
-      {children}
-    </button>
-  );
-}
-
-function PressButton3({ onPress, children }) {
-  // Exposes an `onBuy` prop (React Native style) and maps it to a web `onClick`.
-  return (
-    <button type="button" onClick={onPress}>
-      {children}
-    </button>
-  );
-}
 
 function App() {
 
@@ -37,29 +22,6 @@ function App() {
 
   const handlePress = () => setPoints((p) => p + x);
 
-  const handlePressAuto = () => setPoints((p) => p + 1);
-
-  const handlePress2  = () => {
-    if (points >= priceofItem2) {
-      setPoints((p) => p - priceofItem2);
-      alert('Item purchased!');
-      x = x+1;
-      priceofItem2 = Math.round(priceofItem2 *1.15);
-    } else {
-      alert('Not enough points to buy this item.');
-    }
-  };
-
-  const handleBuy = () => {
-    if (points >= priceofItem) {
-      setPoints((p) => p - priceofItem);
-      alert('Item purchased!');
-      setInterval(handlePressAuto, 3000)
-      priceofItem = Math.round(priceofItem *1.15);
-    } else {
-      alert('Not enough points to buy this item.');
-    }
-  };
   return (
     <div className="App">
       <header className="App-header">
@@ -68,12 +30,12 @@ function App() {
           <PressButton onPress={handlePress}>
             Click!!
           </PressButton>
-          <PressButton2 onPress={handlePress2}>
+          <PressButton onPress={() => Shop({ itemID: 1, points, setPoints, priceofItem: priceofItem2 })}>
             Double click power ({priceofItem2} points)
-          </PressButton2>
-          <PressButton3 onPress={handleBuy}>
+          </PressButton>
+          <PressButton onPress={() => Shop({ itemID: 2, points, setPoints, priceofItem: priceofItem })}>
             Buy item ({priceofItem} points)
-          </PressButton3>
+          </PressButton>
         </p>
       </header>
     </div>
